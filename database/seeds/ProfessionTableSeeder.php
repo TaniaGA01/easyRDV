a28fe8c08cbd4165111cea7793b1c022f015d6e0
David CUOMO <dcuomo@FORMATION.lan>
2019-11-21 15:37:54 +0100

    Update Seeder profession

--- database/seeds/ProfessionTableSeeder.php
+++ database/seeds/ProfessionTableSeeder.php
@@ -13,58 +13,63 @@ class ProfessionTableSeeder extends Seeder
     public function run()
     {
         DB::table('professions')->insert([
-            'name' => 'Neurologue',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Neuro-compresseur',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Psychologue',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Psychopathe',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Chasseur de primes',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Laveur de cerveau',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Avocat',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Baveux',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Ramasseur de champignons',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Flyfu**er',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Chasseur de Troll',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Moine à temps partiel',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Socialiste dépressif',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Barbier',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Stagiaire spécialisé Maison du Café',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Web designer',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Péripapétitien*ne',
-        ]);
-        DB::table('professions')->insert([
-            'name' => 'Développeur Web',
+            [
+                'name' => 'Neurologue'
+            ],
+            [
+                'name' => 'Neuro-compresseur'
+            ],
+            [
+                'name' => 'Psychologue'
+            ],
+            [
+                'name' => 'Comptable'
+            ],
+            [
+                'name' => 'Baveux'
+            ],
+            [
+                'name' => 'Psychopathe'
+            ],
+            [
+                'name' => 'Ramasseur de champignons'
+            ],
+            [
+                'name' => 'Flyfu**er'
+            ],
+            [
+                'name' => 'Moine à temps partiel'
+            ],
+            [
+                'name' => 'Socialiste dépressif'
+            ],
+            [
+                'name' => 'Chasseur de Troll'
+            ],
+            [
+                'name' => 'Avocat'
+            ],
+            [
+                'name' => 'Barbier'
+            ],
+            [
+                'name' => 'Stagiaire spécialisé Maison du Café'
+            ],
+            [
+                'name' => 'Web designer'
+            ],
+            [
+                'name' => 'Péripapétitien*ne'
+            ],
+            [
+                'name' => 'Développeur Web'
+            ],
+            [
+                'name' => 'Laveur de cerveau'
+            ],
+            [
+                'name' => 'Chasseur de primes'
+            ]
         ]);
     }
 }
