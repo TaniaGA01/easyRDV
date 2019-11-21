137a7e9eee16f337477088809236c7eeb47c62a3
David CUOMO <dcuomo@FORMATION.lan>
2019-11-21 14:31:31 +0100

    Update agenda Table

---
+++ database/migrations/2019_11_21_131132_create_agendas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_users');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
