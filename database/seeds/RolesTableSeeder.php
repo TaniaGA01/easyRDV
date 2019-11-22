dc8280290adaa0ebc9a24183c0ac9ef91e161d80
David CUOMO <dcuomo@FORMATION.lan>
2019-11-22 10:17:56 +0100

    Seeder Roles Table & Màj Database Seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            [
+                'name' => 'admin'
+            ],
+            [
+                'name' => 'professionnel'
+            ],
+            [
+                'name' => 'utilisateur'
+            ]
+        ]);
+    }
+}
