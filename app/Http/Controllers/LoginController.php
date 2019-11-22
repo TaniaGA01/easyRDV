66a4356f278c3a4b3db670441598679e1d37ae64
Erwan RIAUX <eriaux@FORMATION.lan>
2019-11-22 16:56:26 +0100

    validate form create new user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -30,6 +30,16 @@ class LoginController extends Controller
             'email_register' => 'required | email | unique:users,email',
             'password_register' => 'required | min:6 | confirmed',
             'cgu_register' => 'accepted'
+        ],[
+            'email_register.required' => 'Saisissez votre adresse e-mail.',
+            'email_register.email' => 'Adresse e-mail non valide.',
+            'email_register.unique' => 'Adresse e-mail déjà utilisée.',
+
+            'password_register.required' => 'Entrez votre mot de passe.',
+            'password_register.min' => 'Les mots de passe doivent au moins avoir 6 caractères.',
+            'password_register.confirmed' => 'Les mots de passe ne correspondent pas.',
+
+            'cgu_register.accepted' => 'Vous devez accepter les Conditions Générales d\'Utilisation.'
         ]);
 
         // Envoi dans la BDD
