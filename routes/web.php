eb7deb0554242e5bb4f82cc34fff636810017270
ErwanR <erwanr@protonmail.com>
2019-12-01 12:43:05 +0100

    add routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,58 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Page d'accueil/recherche
+Route::view('/','welcome');
+Route::post('/','HomeController@searchPro')->name('searchpro');
+Route::get('/liste-des-professionnels/{page}/{field}/{city}','HomeController@index')->name('index');
+Route::get('/{name}/{city}/{profession}','HomeController@show')->name('show');
+
+// Page "A propos"
+Route::get('/a-propos', 'AboutController@index')->name('about');
+
+// Page "Tarif"
+Route::get('/tarifs', 'PriceController@index')->name('price');
+
+// Page "Nous contacter"
+Route::get('nous-contacter','ContactController@create')->name('contact.create');
+Route::post('nous-contacter','ContactController@store')->name('contact.store');
+
+// Page "Je suis un professionnel"
+Route::get('je-suis-un-professionnel','ProfessionalController@create')->name('professional.create');
+Route::post('je-suis-un-professionnel','ProfessionalController@store')->name('professional.store');
+
+// Page "Se connecter/S'inscrire" Javier
+Route::get('connexion-inscription','LoginController@create')->name('login');
+Route::post('connexion','LoginController@store')->name('login.store');
+
+Route::post('inscription','InscriptionController@store')->name('inscription.store');
+
+// Page "Information légales"
+Route::get('/information-legales', 'LegalController@index')->name('legal.index');
 
 
+// Espace client
+// Route page d'accueil ???
+
+Route::get('mes-rendez-vous/{name}','ClientController@index')->name('client.index');
+
+Route::get('mes-informations/{name}/editer','ClientController@edit')->name('client.edit');
+Route::put('mes-informations/{name}','ClientController@update')->name('client.update');
+
+// Espace Professionnel
+Route::get('mon-agenda/{name}','ProfessionnelController@indexAgenda')->name('professionnel.indexAgenda');
+// Route formulaire creation rdv perso ???
+Route::post('mon-agenda/{name}','ProfessionnelController@store')->name('professionnel.store');
+
+Route::get('mes-rendez-vous/{name}','ProfessionnelController@indexAppointment')->name('professionnel.indexAppointment');
+
+Route::get('mes-informations/{name}/editer','ProfessionnelController@edit')->name('professionnel.edit');
+Route::put('mes-informations/{name}','ProfessionnelController@update')->name('professionnel.update');
+
+
+
+
+//#######################################################
 // Contact
 Route::get('contact','ContactController@create');
 Route::post('contact','ContactController@store');
