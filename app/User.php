e6c4ac20bb7282d463e6f1fecf292bf0c32b4c8d
DavidC212 <57533092+DavidC212@users.noreply.github.com>
2019-12-01 16:44:06 +0100

    Modif dans le Model User (à vérifier)

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profession()
+    {
+        return $this->belongsTo('App\Profession');
+    }
 }
