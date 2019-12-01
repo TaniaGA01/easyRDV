e1b9342d2177bcf7b66ac0efb42c378e125a442b
DavidC212 <57533092+DavidC212@users.noreply.github.com>
2019-12-01 16:42:31 +0100

    Màj vue temporaire

--- resources/views/temporaire.blade.php
+++ resources/views/temporaire.blade.php
@@ -2,9 +2,43 @@
 
 @section('content')
 <div>On teste la page temporaire</div>
-<div class="form-container">
-    <label for="pros">Tapez le nom d'un pays</label> :
+<form class="form-container" action="{{route('results', '@')}}" method="GET">
+    <label for="pros">Entrez le nom d'un professionnel ou d'une profession</label> :<br />
     <input type="text" id="pros">
+    <button type="submit">Rechercher</button>
     <div id="suggestions"></div>
-</div>
+</form>
+
+<section style="float:right;width:60vw;">
+    @isset($results)
+
+    @foreach ($results as $result)
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$result->first_name}} {{$result->last_name}}</h5>
+                        {{-- <p class="card-text">{{$results->first_name}}</p> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    @endisset
+
+    @isset($nope)
+        <div>{{$nope}}</div>
+    @endisset
+
+    {{-- @isset($nope)
+    <div>{{$nope}}</div>
+    @endisset --}}
+</section>
+
+
+
 @endsection
