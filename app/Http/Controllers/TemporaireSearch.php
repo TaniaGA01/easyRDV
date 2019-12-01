5492a829be4b63210c09122c40d14b913071f6be
DavidC212 <57533092+DavidC212@users.noreply.github.com>
2019-12-01 17:57:10 +0100

    Màj Controller Temporaire (ça marche) Ça marche, par contre le js marche plus, donc plus d'autocomplétion...

--- app/Http/Controllers/TemporaireSearch.php
+++ app/Http/Controllers/TemporaireSearch.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Profession;
+use App\User;
 
 
 class TemporaireSearch extends Controller
@@ -13,7 +14,34 @@ class TemporaireSearch extends Controller
     }
 
     public function tableau(){
-        $pros = Profession::all();
-        return response()->json($pros);
+        $jobs = Profession::all();
+        $pros = User::all();
+        $tab = [];
+        array_push($tab, $jobs, $pros);
+        return response()->json($tab);
+        // $pros = Profession::all();
+        // return response()->json($pros);
+    }
+
+    public function results($name){
+        // $profession = Profession::find($name);
+        $profession = Profession::where('name',$name)->get();
+        // return $profession;
+
+        $id_profession = $profession[0]->id;
+
+        // return $id_profession;
+
+        $results = User::where('profession_id',$id_profession)->get();
+        if (isset($results)) {
+            return view('temporaire',[
+                'results'=> $results,
+            ]);
+        }else {
+            $nope = "Aucun résultat pour cette recherche";
+            return view('temporaire', [
+                'nope' => $nope,
+            ]);
+        }
     }
 }
