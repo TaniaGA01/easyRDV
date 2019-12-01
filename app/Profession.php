898aa619252363d9d477d15d82473c2b9ed27829
DavidC212 <57533092+DavidC212@users.noreply.github.com>
2019-12-01 16:43:29 +0100

    Modif dans le Model Profession (à vérifier)

--- app/Profession.php
+++ app/Profession.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Profession extends Model
 {
     protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->hasMany('App\User');
+    }
+
 }
