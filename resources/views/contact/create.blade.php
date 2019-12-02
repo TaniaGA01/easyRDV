6f52b9b3fab9834392fc83019a3474a926dfbd62
ErwanR <erwanr@protonmail.com>
2019-12-02 21:54:15 +0100

    Form Contact - Boostrap

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -1,31 +1,85 @@
 @extends('layout')
 
 @section('content')
-<div class="form-style-5">
-    <form action="/contact" method="POST">
-        @csrf
-        <fieldset>
-            <legend>Nous contacter</legend>
-            <!-- si le champ n'est pas rempli on affiche un message et on ajoute la classe bg-red -->
-            @error('last_name'){{ $errors->first('last_name') }}@enderror
-            <!-- {{ old('last_name') }} : si problème lors de la validation on garde la valeur écrite par l'utilisateur -->
-            <input @error('last_name') class="bg-red" @enderror type="text" name="last_name"
-                placeholder="Nom (obligatoire)" value="{{ old('last_name') }}">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Nous contacter</div>
 
-            <input type="text" name="first_name" placeholder="Prenom (facultatif)" value="{{ old('first_name') }}">
+                <div class="card-body">
+                    <form action="/contact" method="POST">
+                        @csrf
 
-            @error('email'){{ $errors->first('email') }}@enderror
-            <input @error('email') class="bg-red" @enderror type="email" name="email" placeholder="Email (obligatoire)"
-                value="{{ old('email') }}">
+                        <!-- si le champ n'est pas rempli on affiche un message et on ajoute la classe bg-red -->
+                       
+                        <!-- {{ old('last_name') }} : si problème lors de la validation on garde la valeur écrite par l'utilisateur -->
 
-            <input type="text" name="phone" placeholder="Téléphone (facultatif)" value="{{ old('phone') }}">
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <input type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" autocomplete="name" autofocus placeholder="Nom (obligatoire)">
 
-            @error('content'){{ $errors->first('content') }}@enderror
-            <textarea @error('content') class="bg-red" @enderror name="content" cols="30" rows="10"
-                placeholder="Message (obligatoire)">{{ old('content') }}</textarea>
+                                @error('last_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('last_name') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
-        </fieldset>
-        <input type="submit" value="Envoyer le message"></button>
-    </form>
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <input type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" autocomplete="given-name" autofocus placeholder="Prénom (facultatif)">
+
+                                @error('first_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('first_name') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus placeholder="Email (obligatoire)">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <input type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" autocomplete="tel" autofocus placeholder="Téléphone (facultatif)">
+
+                                @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <textarea rows="5" class="form-control @error('content') is-invalid @enderror" name="content" autofocus placeholder="Message (obligatoire)">{{ old('content') }}</textarea>
+
+                                @error('content')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('content') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <input type="submit" class="btn btn-block btn-primary" value="Envoyer le message"></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
