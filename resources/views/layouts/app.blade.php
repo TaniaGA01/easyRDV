7ac5d22ae0706cff86f3f3c1bcfddd06683a7560
Javier PEREZ MOTA <jperezmota@FORMATION.lan>
2019-12-03 15:37:54 +0100

    Modification URL footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('img/logo-easy-rdv.svg') }}" alt="{{ config('app.name', 'Laravel') }}" width="150">    
+                    <img src="{{ asset('img/logo-easy-rdv.svg') }}" alt="{{ config('app.name', 'Laravel') }}" width="150">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -88,7 +88,7 @@
             <li><a href="#">A propos</a></li>
             <li><a href="#">Tarifs</a></li>
             <li><a href="{{route('contact.create')}}">Nous contacter</a></li>
-            <li><a href="#">Informations légales</a></li>
+            <li><a href="{{route('legal.index')}}">Informations légales</a></li>
         </ul>
     </nav>
 </body>
