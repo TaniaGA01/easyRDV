bd3344ec0e81b4207c1d8641f603088d40dee9bc
Tania Gaitán <taniagaitan@Tanias-MacBook-Pro.local>
2019-12-03 16:34:19 +0100

    creation style.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="container ptb-6">
         <div class="row">
-            <div class="form-style-5 bg-white shadow-sm col-4 p-6">
+            <div class="form-style-5 bg-white shadow-sm col-4 px-6 py-6">
                 <form action="/" method="post">
                     <fieldset>
                         <legend>Prendre un rdv en ligne</legend>
                         <p>Rechercher un professionnel</p>
-                        <input type="text" name="professional" placeholder="Professionnel" class="form-control">
-                        <input type="text" name="city" placeholder="Lieu"class="form-control">
-                        <input type="submit" value="Rechercher"class="btn btn-primary btn-md col-md-7"></button>
+                        <input type="text" name="professional" placeholder="Professionnel" class="form-control my-2">
+                        <input type="text" name="city" placeholder="Lieu"class="form-control ">
+                        <input type="submit" value="Rechercher"class="btn-pr col-md-12 my-2"></button>
                     </fieldset>
                 </form>
             </div>
