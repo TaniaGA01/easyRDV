ddfd83554dd59c9fba5beae5746f47d75e04f4de
Tania Gaitán <taniagaitan@Tanias-MacBook-Pro.local>
2019-12-04 15:19:52 +0100

    update styles

--- resources/views/pages/Apropos.blade.php
+++ resources/views/pages/Apropos.blade.php
@@ -5,22 +5,24 @@
 
 @section('content')
 
-<div class="container">
+<div class="container ptb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        À propos
+                    </div>
 
-<div class="card">
+                    <div class="card-body">
 
-    <div class="card-header">
-       <h3>À PROPOS</h3>
+                        <p class="card-text">Easyrdv.fr est une plateforme en ligne permettant la gestion des rdv, destinée aux professionnels n’ayant pas encore de solution adaptée.</p>
+                        <p class="card-text">Elle permet également aux clients de ces professionnels de prendre rendez-vous facilement.
+                        Pas de logiciel ? Pas d’assistant ou d’employé ? Pas le temps d’apprendre à gérer un nouveau système ? </p>
+                        <p class="card-text">Easyrdv.fr est justement conçu pour une prise en main facile, en quelques clics, aussi bien pour les professionnels que leurs clients.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="card-body">
-
-        <p class="card-text">Easyrdv.fr est une plateforme en ligne permettant la gestion des rdv, destinée aux professionnels n’ayant pas encore de solution adaptée.
-                             Elle permet également aux clients de ces professionnels de prendre rendez-vous facilement.
-                             Pas de logiciel ? Pas d’assistant ou d’employé ? Pas le temps d’apprendre à gérer un nouveau système ? Easyrdv.fr est justement conçu pour une prise en main facile, en quelques clics, aussi bien pour les professionnels que leurs clients.</p>
-</div>
-  </div>
-
-</div>
 @endsection
 
--- resources/views/pages/informationsLegales.blade.php
+++ resources/views/pages/informationsLegales.blade.php
@@ -3,25 +3,25 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="container ptb-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            Informations légales
+                        </div>
 
-<div class="container">
+                        <div class="card-body">
 
-<div class="card">
-
-    <div class="card-header">
-       <h3>INFORMATIONS LEGALES</h3>
+                            <p class="card-text">Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l’égard des traitements de données à caractère
+                            l’informatique, aux fichiers et aux libertés, ce site a fait l’objet d’une déclaration 1656629 auprès de la
+                            Commission nationale de l’informatique et des libertés <a href="http://www.cnil.fr">(www.cnil.fr).</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="card-body">
-
-        <p class="card-text">Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 <br>
-        relative à la protection des personnes physiques à l’égard des traitements de données à caractère <br>
-        l’informatique, aux fichiers et aux libertés, ce site a fait l’objet d’une déclaration 1656629 auprès de la <br>
-        Commission nationale de l’informatique et des libertés <a href="http://www.cnil.fr">(www.cnil.fr).</a></p>
-
-    </div>
-  </div>
-
-</div>
 @endsection
 
