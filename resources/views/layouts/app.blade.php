38ce661921f5083b87340b2d85b30090f2a03142
Erwan RIAUX <eriaux@FORMATION.lan>
2019-12-05 12:28:32 +0100

    espace pro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    @if(Auth::user()->role_id == 2)
+                                    <a class="dropdown-item" href="{{ route('professionnelArea.indexAgenda', Auth::user()->first_name) }}">Mon agenda</a>
+                                    @endif
                                     <a class="dropdown-item" href="#">Mes rendez-vous</a>
                                     <a class="dropdown-item" href="#">Mes infos perso</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
