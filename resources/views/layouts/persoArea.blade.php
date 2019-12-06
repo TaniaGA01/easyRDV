0ab1b07ae20bca7555d6931e11601ea21aeeb74c
Erwan RIAUX <eriaux@FORMATION.lan>
2019-12-06 17:19:29 +0100

    fix btn si pro ou client

--- resources/views/layouts/persoArea.blade.php
+++ resources/views/layouts/persoArea.blade.php
@@ -7,11 +7,11 @@
                 <img src="{{$user->image}}" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title">{{ $user->first_name }} {{ Str::upper($user->last_name) }}</h5>
-                    @if($user->id == 2)
+                    @if($user->role_id == 2)
                     <p class="card-text">{{ $user->profession->name }}</p>
                     @endif
                 </div>
-                @if($user->id == 2)
+                @if($user->role_id == 2)
                 <a href="{{ route('professionnelArea.indexAgenda', Auth::user()->id) }}" class="btn btn-pr btn-block">Mon agenda</a>
                 @endif
                 <a href="#" class="btn btn-pr btn-block">Mes rendez-vous</a>
