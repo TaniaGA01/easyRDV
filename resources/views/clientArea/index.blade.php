5bfbfc5bf1fb5ea66df77219d6fdb009edf2fc69
ErwanR <erwanr@protonmail.com>
2019-12-08 10:57:16 +0100

    exemple rendez-vous du client

--- resources/views/clientArea/index.blade.php
+++ resources/views/clientArea/index.blade.php
@@ -10,6 +10,56 @@
         </div>
     </div>
     @endif
-   <h1>Page : Mes rendez-vous (client)</h1> 
-</div>
-@endsection
+    <div class="row justify-content-center">
+        <div class="col-md-11">
+            <div class="card">
+                <div class="card-body ">
+
+
+                    <h5 class="card-title">Prochains rendez-vous</h5>
+                    <div class="row">
+                        <div class="card col-md-5">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nom Prénom</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Métier</h6>
+                                <hr>
+                                <p class="card-text"><i class="far fa-calendar-alt"></i> Date du rdv</p>
+                                <p class="card-text"><i class="fas fa-map-marker"></i> Adresse</p>
+                                <p class="card-text"><i class="fas fa-phone"></i> Téléphone</p>
+                                <p class="card-text"><i class="fas fa-globe"></i> Site web</p>
+                                <div class="form-group justify-content-center row">
+                                <input type="submit" class="btn-pr btn-block " value="Annuler rendez-vous"></button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <h5 class="card-title">Rendez-vous passés</h5>
+                    <div class="row">
+                        <div class="card col-md-5">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nom Prénom</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Métier</h6>
+                                <hr>
+                                <p class="card-text"><i class="far fa-calendar-alt"></i> Date du rdv</p>
+                                <p class="card-text"><i class="fas fa-map-marker"></i> Adresse</p>
+                                <p class="card-text"><i class="fas fa-phone"></i> Téléphone</p>
+                                <p class="card-text"><i class="fas fa-globe"></i> Site web</p>
+                                <div class="form-group justify-content-center row">
+                                <input type="submit" class="btn-pr btn-block " value="Reprendre rendez-vous"></button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
