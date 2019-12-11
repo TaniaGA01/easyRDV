e2db966e93b503ba75cf3a13386f83c1cc51d7fe
Tania Gaitán <taniagaitan@tanias-macbook-pro.home>
2019-12-11 21:55:32 +0100

    Update - Espace client

--- resources/views/clientArea/index.blade.php
+++ resources/views/clientArea/index.blade.php
@@ -2,7 +2,7 @@
 @section('contentPagePerso')
 <div class="container">
     @if (session('status'))
-    <div class="row justify-content-center">
+    <div class="row justify-content-center ">
         <div class="col-md-12">
             <div class="alert {{ session('alert-class') }}" role="alert">
                 <i class="fas fa-check-circle"></i> {{ session('status') }}
@@ -10,75 +10,103 @@
         </div>
     </div>
     @endif
-    <div class="row">
-        <div class="form-style-5 bg-white shadow-sm col-md-12 px-5 py-5">
+    <div class="row justify-content-center">
+        <div class="my-appointments bg-white shadow-sm col-md-12 px-5 py-5">
             @if(count($tab_appointments)>0)
-            <h5 class="card-title">Prochains rendez-vous</h5>
-            <div class="row">
-                @php $i = 0; @endphp
-                @foreach($tab_appointments as $tab_appointment)
-                <div class="card col-md-5">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">{{ $tab_appointments[$i]['name'] }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted font-weight-bold">
-                            {{ $tab_appointments[$i]['profession'] }}</h6>
-                        <hr>
 
-                        <p class="card-text font-weight-bold"><i class="far fa-calendar-alt"></i>
-                            {{ $tab_appointments[$i]['date'][0] }} à {{ $tab_appointments[$i]['date'][1] }}h</p>
-                        <p class="card-text"><i class="fas fa-map-marker"></i>
-                            {{ $tab_appointments[$i]['address'] }}<br>
-                            {{ $tab_appointments[$i]['city'] }}</p>
-                        <p class="card-text"><i class="fas fa-phone"></i> {{ $tab_appointments[$i]['phone'] }}</p>
-                        <!--
-                            <p class="card-text"><i class="fas fa-globe"></i> Site web</p>
-                            -->
-                        <div class="form-group justify-content-center row">
-                        <input type="submit" class="btn-pr btn-block data-rdv page-pro rdv-loaded rdv-annul page-client" data-client="{{Auth::user()->id}}" data-id="{{ $tab_appointments[$i]['id_rdv'] }}" data-tartempion="{{ $tab_appointments[$i]['data_tartempion'] }}" data-name="{{ $tab_appointments[$i]['name'] }}" data-token="<?php echo csrf_token();?>" value="Annuler le rendez-vous">
+            <div class="container">
+                    <h2>Prochains rendez-vous</h2>
+                <div class="row">
+                    @php $i = 0; @endphp
+                    @foreach($tab_appointments as $tab_appointment)
+                    <div class="col-md-6">
+                        <div class="rdv bg-white shadow">
+                            <div class="rdvInfo">
+                                <div class="row">
+                                    <div class="col-3">
+                                        <img src="https://picsum.photos/50/50" class="" alt="..." width="50" height="50">
+                                    </div>
+                                    <div class="col-9">
+                                        <h5>{{ $tab_appointments[$i]['name'] }}</h5>
+                                        <h6>{{ $tab_appointments[$i]['profession'] }}</h6>
+                                    </div>
+                                </div>
+                                <hr>
+
+                                <p class="card-text font-weight-bold"><i class="far fa-calendar-alt"></i>
+                                    {{ $tab_appointments[$i]['date'][0] }} à {{ $tab_appointments[$i]['date'][1] }}h</p>
+                                <p class="card-text"><i class="fas fa-map-marker"></i>
+                                    {{ $tab_appointments[$i]['address'] }}<br>
+                                    {{ $tab_appointments[$i]['city'] }}</p>
+                                <p class="card-text"><i class="fas fa-phone"></i> {{ $tab_appointments[$i]['phone'] }}
+                                </p>
+                                <input type="submit"
+                                class="btn-pr btn-block data-rdv page-pro rdv-loaded rdv-annul page-client"
+                                data-client="{{Auth::user()->id}}" data-id="{{ $tab_appointments[$i]['id_rdv'] }}"
+                                data-tartempion="{{ $tab_appointments[$i]['data_tartempion'] }}"
+                                data-name="{{ $tab_appointments[$i]['name'] }}" data-token="<?php echo csrf_token();?>"
+                                value="Annuler le rendez-vous">
+                            </div>
+
                         </div>
                     </div>
+                    @php $i++; @endphp
+                    @endforeach
                 </div>
-                @php $i++; @endphp
-                @endforeach
             </div>
             @else
-            <h5 class="card-title">Vous n'avez pas de rendez-vous actuellement.</h5>
-            @endif
-            @if(count($tab_appointments_before)>0)
-            <h5 class="card-title">Rendez-vous passés</h5>
-            <div class="row">
-                @php $j = 0; @endphp
-                @foreach($tab_appointments_before as $tab_appointment_before)
-                <div class="card col-md-5">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">{{ $tab_appointments_before[$j]['name'] }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $tab_appointments_before[$j]['profession'] }}</h6>
-                        <hr>
-                        <p class="card-text font-weight-bold"><i class="far fa-calendar-alt"></i> {{ $tab_appointments_before[$j]['date'][0] }} à {{ $tab_appointments_before[$j]['date'][1] }}h</p>
-                        <p class="card-text"><i class="fas fa-map-marker"></i> {{ $tab_appointments_before[$j]['address'] }}<br>
-                            {{ $tab_appointments_before[$j]['city'] }}</p></p>
-                        <p class="card-text"><i class="fas fa-phone"></i> {{ $tab_appointments_before[$j]['phone'] }}</p>
-                        <!--
-                        <p class="card-text"><i class="fas fa-globe"></i> Site web</p>
-                        -->
-                        <!-- Route::get('/professionnels/{profession}/{city}/{first_name}_{last_name}','HomeController@show')->name('show'); -->
-                        <div class="form-group justify-content-center row">
-                            <!-- <input type="submit" class="btn-pr btn-block " value="Reprendre un rendez-vous"></button> -->
-                            <a href="{{ route('show', [
-                                'profession' => $tab_appointments_before[$j]['profession'],
-                                'city' => str_replace(" ","-",$tab_appointments_before[$j]['city']),
-                                'first_name' => $tab_appointments_before[$j]['first_name'],
-                                'last_name' => str_replace(" ","-",$tab_appointments_before[$j]['last_name'])
-                                ]) }}" class="btn btn-pr btn-block">Reprendre un rendez-vous</a>
+            <h2>Vous n'avez pas de rendez-vous actuellement.</h2>
+            @endif</div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="my-past-appointments bck-bleu shadow-sm col-md-12 px-5 py-5">
+                @if(count($tab_appointments_before)>0)
+                <div class="container">
+                <h2>Rendez-vous passés</h2>
+                    <div class="row">
+                        @php $j = 0; @endphp
+                        @foreach($tab_appointments_before as $tab_appointment_before)
+                        <div class="col-md-6">
+                            <div class="rdv bg-white shadow">
+                                <div class="rdvInfo">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <img src="https://picsum.photos/50/50" class="" alt="..." width="50" height="50">
+                                        </div>
+                                        <div class="col-9">
+                                            <h5 class="card-title font-weight-bold">
+                                                {{ $tab_appointments_before[$j]['name'] }}</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">
+                                                {{ $tab_appointments_before[$j]['profession'] }}</h6>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <p class="card-text font-weight-bold"><i class="far fa-calendar-alt"></i>
+                                        {{ $tab_appointments_before[$j]['date'][0] }} à
+                                        {{ $tab_appointments_before[$j]['date'][1] }}h</p>
+                                    <p class="card-text"><i class="fas fa-map-marker"></i>
+                                        {{ $tab_appointments_before[$j]['address'] }}<br>
+                                        {{ $tab_appointments_before[$j]['city'] }}</p>
+                                    </p>
+                                    <p class="card-text"><i class="fas fa-phone"></i>
+                                        {{ $tab_appointments_before[$j]['phone'] }}</p>
+
+                                    <a href="{{ route('show', [
+                                        'profession' => $tab_appointments_before[$j]['profession'],
+                                        'city' => str_replace(" ","-",$tab_appointments_before[$j]['city']),
+                                        'first_name' => $tab_appointments_before[$j]['first_name'],
+                                        'last_name' => str_replace(" ","-",$tab_appointments_before[$j]['last_name'])
+                                        ]) }}" class="btn btn-pr btn-block">Reprendre un rendez-vous
+                                    </a>
+                                </div>
+                            </div>
                         </div>
+                        @php $j++; @endphp
+                        @endforeach
                     </div>
                 </div>
-                @php $j++; @endphp
-                @endforeach
-            </div>
-            @endif
+                @endif
         </div>
     </div>
-    @endsection
+</div>
+@endsection
