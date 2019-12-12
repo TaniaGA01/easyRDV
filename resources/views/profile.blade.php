07b2fee5594338eefb78e8231365ca0bb340c237
Javier PEREZ MOTA <jperezmota@FORMATION.lan>
2019-12-12 12:08:18 +0100

    UpDate page profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,13 +5,13 @@
 <div class="container ptb-5">
         <div class="row">
             @auth
-                
-           
+
+
             <div class="form-style-5 bg-white shadow-sm col-12 px-6 py-6">
                     <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
                     {{-- <h2>{{ $user->name }}Profile</h2> --}}
                     <form enctype="multipart/form-data" action="/profile" method="POST">
-                        <label>Update Profile Image</label>
+                        <label>Selectionner une image de profile</label>
                         <input type="file" name="avatar">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="submit" class="pull-right btn btn-sm btn-primary">
@@ -20,9 +20,9 @@
         </div>
     </div>
     @endauth
-                
-         
-    
+
+
+
 
 @endsection
 
