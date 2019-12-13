a527eea73fc03518b5f48d8264e55b4a79fe9f2c
Javier PEREZ MOTA <jperezmota@FORMATION.lan>
2019-12-13 17:29:29 +0100

    Mise à jour poage 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,22 @@
 
 
 @extends('layouts.app')
-@extends('errors::layout')
 
-@section('title', 'Not Found')
-@section('message', 'OUPS !  LA PAGE QUE VOUS CHERCHEZ ET INTROUVABLE.')
+@section('content')
+
+@section('title', 'Page introuvable')
+
+<div class="container ptb-5 position-relative"><br> <br>
+    <div class="my-appointments bg-white shadow-sm col-md-12 px-5 py-5 ptb-5 m-auto">
+        <h1 class="text-center">Oups ! <br>
+        La page que vous cherchez n'existe pas.</h1>
+    </div>
+
+</div>
+
+
+@endsection
+
+
 
 
