d05a76763adb4fd2c7c1200afb460f37a10ca8a8
ErwanR <erwanr@protonmail.com>
2019-12-13 21:01:29 +0100

    personalisation des mails

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 Bonjour !
 
-Veuillez cliquer sur le bouton ci-dessous pour vérifier votre adresse e-mail.
+Veuillez cliquer sur le bouton ci-dessous pour vérifier votre adresse mail.
 
 @component('mail::button', ['url' => $url])
-Vérifier l'adresse e-mail
+Vérifier l'adresse mail
 @endcomponent
 Si vous n'avez pas créé de compte, aucune autre action n'est requise.
 
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}">
+            <img src="{{ asset('img/logo-easy-rdv.svg') }}" alt="Logo {{ config('app.name', 'Laravel') }}" width="150">
+        </a>
+    </td>
+</tr>
