eac48bb8ab79e42dc9b9b938a064b64e1cd98fdd
Javier PEREZ MOTA <jperezmota@FORMATION.lan>
2019-12-13 17:02:59 +0100

    UpDate Page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,9 @@
 
 
-
+@extends('layouts.app')
 @extends('errors::layout')
 
 @section('title', 'Not Found')
 @section('message', 'OUPS !  LA PAGE QUE VOUS CHERCHEZ ET INTROUVABLE.')
 
-@extends('layouts.app')
+
