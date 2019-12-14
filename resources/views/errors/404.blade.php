1ea5299cece7c96c0a38aa2ba12ebd9b34462f86
ErwanR <erwanr@protonmail.com>
2019-12-14 13:55:49 +0100

    amélioration page 404, #todo class

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,20 +1,16 @@
-
-
 @extends('layouts.app')
 
-@section('content')
 
+@section('content')
 @section('title', 'Page introuvable')
 
-<div class="container ptb-5 position-relative"><br> <br>
-    <div class="my-appointments bg-white shadow-sm col-md-12 px-5 py-5 ptb-5 m-auto">
-        <h1 class="text-center">Oups ! <br>
-        La page que vous cherchez n'existe pas.</h1>
+<div style="background-color: var(--orange-clair); height: 56vh">
+    <div class="container ptb-5 position-relative"><br><br>
+        <div class="my-appointments bg-white shadow-sm col-md-12 px-5 py-5 ptb-5">
+            <h1 class="text-center" style="color: var(--bleu);">Oups ! La page que vous cherchez n'existe pas.</h1>
+        </div>
     </div>
-
 </div>
-
-
 @endsection
 
 
