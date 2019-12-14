2f0707c7e5879f6746a780d26cde070d7d3769e9
ErwanR <erwanr@protonmail.com>
2019-12-14 10:39:27 +0100

    amélioration template mail

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,14 +1,13 @@
 @component('mail::message')
 <h3>Bonjour !</h3>
 
-Veuillez cliquer sur le bouton ci-dessous pour vérifier votre adresse mail.
+Veuillez cliquer sur le bouton ci-dessous pour activer votre compte Easy RDV.
 
 @component('mail::button', ['url' => $url])
 Vérifier l'adresse mail
 @endcomponent
-Si vous n'avez pas créé de compte, aucune autre action n'est requise.
+Si vous n'avez pas créé de compte, il vous suffit de supprimer ce mail et tout sera comme avant.
 
 Cordialement,<br>
-{{ config('app.name') }}
-
+Easy RDV
 @endcomponent
